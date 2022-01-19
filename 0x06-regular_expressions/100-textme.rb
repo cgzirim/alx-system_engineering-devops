c9957e4eba66c74d73f19d423b0ca4b732cc10bd
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2022-01-19 06:59:04 -0800

    Make flag plural

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 # Find  [SENDER],[RECEIVER],[FLAGS] in a message transaction log.
 
-puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flag:(.*?)\]/).join
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join
