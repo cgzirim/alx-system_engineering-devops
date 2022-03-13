bc395c84a454ed4974038da16b00fd64b4b7de14
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2022-03-13 20:50:17 +0000

    Correct error

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,15 @@
 # Modifies client configuration file.
-include stdlib
 
 file_line { 'Disable passwd auth':
   ensure  => present,
   path    => '/etc/ssh/sshd_config',
-  line    => '     PasswordAuthentication no',
+  line    => 'PasswordAuthentication no',
   replace => true,
 }
 
 file_line { 'Declare identity file':
   ensure  => present,
   path    => '/etc/ssh/sshd_config',
-  line    => '     IdentityFile ~/.ssh/school',
+  line    => 'IdentityFile ~/.ssh/school',
   replace => true,
 }
