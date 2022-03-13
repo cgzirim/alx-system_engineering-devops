68649138b506df2ab8ae4291114f46e6f9fb7edb
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2022-03-13 20:34:46 +0000

    Modify client configuration with Puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,15 @@
+# Modifies client configuration file.
+
+file_line { 'Disable passwd auth':
+  ensure  => present,
+  path    => '/etc/ssh/sshd_config',
+  line    => 'PasswordAuthentication no',
+  replace => true,
+}
+
+file_line { 'Declare identity file':
+  ensure  => present,
+  path    => '/etc/ssh/sshd_config',
+  line    => '     IdentityFile ~/.ssh/school',
+  replace => true,
+}
