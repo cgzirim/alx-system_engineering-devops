38d4d747ac1d661b6a2d058bb3bec91d3141f113
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2022-04-13 16:34:08 +0000

    Redo task

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
-"""Returns information on TODO list progress for a given employee Id
-using REST API."""
+"""Returns information about a given employee todo list progress."""
 import requests
 from sys import argv
 
 
-if __name__ == "__main__":
-	api_url = "https://jsonplaceholder.typicode.com/"
-	response = requests.get(api_url)
-	print(response.json())
+url = "https://jsonplaceholder.typicode.com/"
+user = requests.get(url + "users/{}".format(argv[1])).json()['name'] 
+tasks = [d for d in requests.get(url + "todos").json() \
+	if d["userId"] == int(argv[1])]
 
-"Employee {} is done with tasks({}/{}):".format()
+tsks_done = 0
+tsks_num = 0
+for t in tasks:
+	tsks_num += 1
+	if t['completed']:
+		tsks_done += 1
+print("Employee {} is done with tasks({}/{}):".format(user, tsks_done,
+tsks_num))
+[print("\t " + t['title']) for t in tasks if t['completed']]
