fefdd3c41f15697f037f19c319ab64ea02a8020d
root <iamokwukwe@gmail.com>
2022-04-13 10:52:05 -0700

    Align code to Pycodestyle

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,12 +5,11 @@ from sys import argv
 
 
 if __name__ == "__main__":
-        url = "https://jsonplaceholder.typicode.com/"
-        user = requests.get(url + "users/{}".format(argv[1])).json()['name']
-        tasks = requests.get(url + "todos", params={"userId": argv[1]}).json()
-        print(requests.get(url + "todos", params={"userId": argv[1]}).url)
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(argv[1])).json()['name']
+    tasks = requests.get(url + "todos", params={"userId": argv[1]}).json()
 
-        completed = [t["title"] for t in tasks if t["completed"] is True]
-        print("Employee {} is done with tasks({}/{}):".format(user,
-        len(completed), len(tasks)))
-        [print("\t {}".format(t)) for t in completed]
+    completed = [t["title"] for t in tasks if t["completed"] is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user, len(completed), len(tasks)))
+    [print("\t {}".format(t)) for t in completed]
