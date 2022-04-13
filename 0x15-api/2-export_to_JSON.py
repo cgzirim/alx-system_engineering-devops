6b39e5e2eb5fe0cd1e12b6bae44be80d81ed2713
root <iamokwukwe@gmail.com>
2022-04-13 14:24:14 -0700

    Write Python script that exports data in JSON format

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Export data in the JSON format."""
+import json
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    user_id = argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    username = requests.get(url + "users/{}".format(
+        argv[1])).json()['username']
+    tasks = requests.get(url + "todos", params={"userId": user_id}).json()
+
+    
+    dicts = []
+    for t in tasks:
+        dicts.append({"task": t['title'], "completed": t['completed'],\
+                "username": username})
+    data = {user_id: dicts}
+
+    jsonString = json.dumps(data)
+    with open("{}.json".format(user_id), 'w') as f:
+        f.write(jsonString)
