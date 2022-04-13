ef82e3d6533e457ea78da2cd5fbebf38cc027688
root <iamokwukwe@gmail.com>
2022-04-13 14:30:23 -0700

    Align code to Pycodestyle

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -12,11 +12,10 @@ if __name__ == "__main__":
         argv[1])).json()['username']
     tasks = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    
     dicts = []
     for t in tasks:
-        dicts.append({"task": t['title'], "completed": t['completed'],\
-                "username": username})
+        dicts.append({"task": t['title'],
+            "completed": t['completed'], "username": username})
     data = {user_id: dicts}
 
     jsonString = json.dumps(data)
