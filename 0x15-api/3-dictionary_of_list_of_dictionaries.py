3b0c9a9668163355b732e5fc669f5e110b008905
root <iamokwukwe@gmail.com>
2022-04-13 15:36:26 -0700

    Export records of all tasks from all employees in JSON format

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Export data in the JSON format."""
+import json
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url + "users").json()
+
+    data = {}
+    for user in users:
+        u_id = user['id']
+        username = user['username']
+        tasks = requests.get(url + "todos", params={'user_Id': u_id}).json()
+
+        dicts = []
+        for t in tasks:
+            dicts.append({"username": username, "task": t['title'],
+                         "completed": t['completed']})
+        data[u_id] = dicts
+
+    jsonString = json.dumps(data)
+    with open("todo_all_employees.json", 'w') as f:
+        f.write(jsonString)
