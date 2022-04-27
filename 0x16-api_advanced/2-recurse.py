008ff1dbc4c6079efa9fd01c6100c7ad9bebda53
root <iamokwukwe@gmail.com>
2022-04-26 17:08:35 -0700

    Correct code

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,7 +3,7 @@
 import requests
 
 
-def recurse(subreddit, hot_list=[], fullname=None, count=0):
+def recurse(subreddit, hot_list=[], fullname=None):
     """Returns a list containing the titles of all hot articles for a given
     subreddit.
     - Returns None if no results are found for the given subreddit.
@@ -12,15 +12,18 @@ def recurse(subreddit, hot_list=[], fullname=None, count=0):
     headers = {'User-Agent': 'MyAPI/0.01'}
 
     response = requests.get(api_url, headers=headers)
+    if fullname is not None:
+         response = requests.get(api_url, headers=headers, params={
+                                'after': fullname})
 
     if response.status_code >= 300:
         return None
 
-    try:
-        post = response.json()['data']['children'][count]
-        hot_list.append(post['data']['title'])
-    except IndexError:
+    post = response.json()['data']['children'][0]
+    hot_list.append(post['data']['title'])
+    fullname = post['kind'] + '_' + post['data']['id']
+
+    if response.get(after) is None:
         return hot_list
 
-    count += 1
-    recurse(subreddit, hot_list, count=count)
+    recurse(subreddit, hot_list, fullname=fullname)
