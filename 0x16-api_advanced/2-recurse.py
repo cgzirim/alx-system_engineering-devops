27804cf0336a8882df24bed63afece2ab856c51c
root <iamokwukwe@gmail.com>
2022-04-28 15:13:30 -0700

    Remove count

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,7 +3,7 @@
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=None, count=0):
+def recurse(subreddit, hot_list=[], after=None):
     """Returns a list containing the titles of all hot articles for a given
     subreddit.
     - Returns None if no results are found for the given subreddit.
@@ -15,8 +15,8 @@ def recurse(subreddit, hot_list=[], after=None, count=0):
     if after is not None:
         response = requests.get(api_url, headers=headers, params={
             'after': after,
-            'count': count,
-            'limit': 100})
+            'limit': 100},
+            allow_redirects=False)
 
     if response.status_code >= 300:
         return None
@@ -24,10 +24,8 @@ def recurse(subreddit, hot_list=[], after=None, count=0):
     for post in response.json()['data']['children']:
         hot_list.append(post['data']['title'])
 
-    count += response.json().get("data").get("dist")
     after = response.json().get("data").get("after")
 
-    if after is None:
-        return hot_list
-
-    recurse(subreddit, hot_list, after=after, count=count)
+    if after is not None:
+        recurse(subreddit, hot_list, after=after)
+    return hot_list
