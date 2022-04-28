edd3f36027a52e0e3224654b877e7ac8246d3c8f
root <iamokwukwe@gmail.com>
2022-04-28 09:45:13 -0700

    Remove test code

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -32,7 +32,3 @@ def recurse(subreddit, hot_list=[], after=None, count=0):
 
     recurse(subreddit, hot_list, after=after, count=count)
 
-
-hot_list = []
-recurse("programming", hot_list)
-print(len(hot_list))
